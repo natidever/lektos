a649bc7a3719c6da4b8f16f63892919aef8cd505
natidever <natnael7820@gmail.com>
2025-07-26 16:41:52 +0300

    limited test runner

--- src/utils/analysis.rs
+++ src/utils/analysis.rs
@@ -9,8 +9,10 @@ use std::io::Result;
 use url::{Host, Position, Url};
 
 use crate::extractors::pipeline::MetadataPipeline;
+use crate::extractors::source_tracker::SourceTracker;
 use crate::models::blog::Blog;
 use crate::utils::html_utils::BlogProcessor;
+use crate::utils::url_visit_check::UrlVisitTracker;
 
 #[derive(Debug, Deserialize)]
 pub struct AnalysisData {
@@ -176,4 +178,78 @@ pub fn url_distribution(value: Vec<AnalysisData>) -> Vec<String> {
 
     //  println!("Found {} duplicated url",duplicated_url);
     list_of_host
+}
+
+
+fn limited_analysis_runner() {
+    let vist_url_tracker = UrlVisitTracker::new();
+
+    let urls = fs::read_to_string("valid_htmls.json").expect("Unable to open html");
+
+    let value: Vec<AnalysisData> = serde_json::from_str(&urls).expect("Failed to serialize-serde");
+
+    let mut json_for_log: Vec<BlogResult> = Vec::new();
+    let mut failed_htmls: Vec<FailedHTML> = Vec::new();
+
+    let mut temp: Vec<String> = Vec::new();
+
+    let mut limit = 0;
+    let mut tracker = SourceTracker::new();
+
+    for data in value {
+        let url_lower = data.url.to_lowercase();
+
+        // Read CV isnot duplicated url rather similar content with different
+        // url till we build content-signature comparision let us remove it manually
+        if url_lower.contains("https://read.cv/") {
+            continue;
+        };
+
+        let file_html = data.html;
+        let pipeline = MetadataPipeline::new();
+        let metadata = pipeline.run(file_html.as_str());
+
+        tracker.record(&metadata);
+
+        let blog_content = BlogProcessor::extract_and_sanitize(file_html.as_str());
+
+        let blog = Blog {
+            title: metadata.title.map(|f| f.value).unwrap_or("Untitled".into()),
+            author: metadata.author.map(|f| f.value).unwrap_or("Unknown".into()),
+
+            date: metadata.date.map(|f| f.value).unwrap_or("".into()),
+            publisher: metadata.publisher.map(|f| f.value).unwrap_or_default(),
+            content: blog_content,
+        };
+
+        segregate_failed_htmls(&blog, &file_html, &mut failed_htmls, &data.url);
+
+        let result = analyze_result(&blog);
+
+        json_for_log.push(result);
+
+        limit += 1;
+
+        // if limit > 200 {
+        //     break;
+        // }
+    }
+    println!("{:#?}", tracker);
+
+    let json_analysis_result =
+        serde_json::to_string_pretty(&json_for_log).expect("Unable to serialize analyzed files");
+    let json_failed_html =
+        serde_json::to_string_pretty(&failed_htmls).expect("Unable to serialize failed html");
+    let json = serde_json::to_string_pretty(&tracker).expect("Failed to serialize tracker");
+
+    let _ = fs::write("analysis_result.json", json_analysis_result)
+        .expect("Unable to write the analsis");
+    let _ =
+        fs::write("failed_html.json", json_failed_html).expect("Unable to write the failed htmls");
+    std::fs::write("source_data_analysis.json", json).expect("Failed to write JSON file");
+
+    println!(
+        "Analyzed {} Files Check the result at analysis_result.json file ",
+        { limit }
+    )
 }
